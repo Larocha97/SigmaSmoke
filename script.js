8db3215c43cdac138e8ebaee909e125d22b55937
Lucas Aparecido Rocha <lucas.rocha@mv.com.br>
2024-08-29 22:05:27 -0300

    v1.1.0

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ const checkoutBtn = document.getElementById("checkout-btn");
 const closeModalBtn = document.getElementById("close-modal-btn");
 const cartCounter = document.getElementById("cart-count");
 const addresInput = document.getElementById("addres");
+const warnTroco = document.getElementById("warn-troco");
+const warnPagamento = document.getElementById("warn-pagamento");
 const addresWarn = document.getElementById("addres-warn");
 const addToCartButtons = document.querySelectorAll('.add-to-card-btn');
+const dinheiroRadio = document.getElementById('dinheiro');
+const trocoInput = document.getElementById('troco-input');
+const paymentOptions = document.querySelectorAll('input[name="payment"]');
+const nomeInput = document.getElementById('nome-input');
+const warnNome = document.getElementById("warn-nome");
+const numberInput = document.getElementById('number');
+const warnNumber = document.getElementById("number-warn");
+const complementoInput = document.getElementById('complemento');
 
 let cart = [];
 
@@ -70,6 +80,19 @@ function addTocart(name, price, sabor) {
         });
     }
     updataCartModal();
+
+    Toastify({
+        text: "Produto adicionado!",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "#ef4444",
+        },
+    }).showToast();
+
 }
 
 //atuliza carrinho
@@ -98,6 +121,10 @@ function updataCartModal() {
         total += item.price * item.quantity;
         cartItemsContainer.appendChild(cartItemElement);
     })
+    window.minhaVariavelGlobal = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
 
     cartTotal.textContent = total.toLocaleString("pt-BR", {
         style: "currency",
@@ -145,50 +172,142 @@ addresInput.addEventListener("input", function (event) {
     }
 })
 
+numberInput.addEventListener("input", function (event) {
+    let inputValue = event.target.value;
+
+    if (inputValue !== "") {
+        numberInput.classList.remove("border-red-500");
+        warnNumber.classList.add("hidden");
+    }
+})
+
+nomeInput.addEventListener("input", function (event) {
+    let inputValue = event.target.value;
+
+    if (inputValue !== "") {
+        warnNome.classList.add("hidden");
+    }
+})
+
+paymentOptions.forEach(option => {
+    option.addEventListener("change", function () {
+        warnPagamento.classList.add("hidden"); // Remove o aviso ao selecionar qualquer opção de pagamento
+    });
+});
+
+
+trocoInput.addEventListener("input", function (event) {
+    let inputValue = event.target.value;
+
+    if (inputValue !== "") {
+        trocoInput.classList.remove("border-red-500");
+        warnTroco.classList.add("hidden");
+    }
+})
+
 checkoutBtn.addEventListener("click", function () {
     const isOpen = checkTabacariaOpen();
-    // if (!isOpen) {
-    //     Toastify({
-    //         text: "Ops estamos fechados!",
-    //         duration: 3000,
-    //         close: true,
-    //         gravity: "top", // `top` or `bottom`
-    //         position: "right", // `left`, `center` or `right`
-    //         stopOnFocus: true, // Prevents dismissing of toast on hover
-    //         style: {
-    //             background: "#ef4444",
-    //         },
-    //     }).showToast();
-    //
-    //     return;
-    // }
+    /* if (!isOpen) {
+        Toastify({
+            text: "Ops estamos fechados!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#ef4444",
+            },
+        }).showToast();
 
-    if (cart.length === 0) return;
+        return;
+    } */
+
+    if (cart.length === 0) {
+        Toastify({
+            text: "Carinho vazio!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#ef4444",
+            },
+        }).showToast();
+        return;
+    }
+
+    const pagamento = verificarPagamentoSelecionado();
+    if (pagamento === null) {
+        warnPagamento.classList.remove("hidden");
+        /* trocoInput.classList.add("border-red-500");  */
+        return;
+    }
+
+    if (pagamento === "dinheiro" && trocoInput.value ==="") {
+        warnTroco.classList.remove("hidden");
+        trocoInput.classList.add("border-red-500"); 
+        return;
+    }
+
     if (addresInput.value === "") {
         addresWarn.classList.remove("hidden");
         addresInput.classList.add("border-red-500");
         return;
     }
 
+    if (numberInput.value === "") {
+        warnNumber.classList.remove("hidden");
+        numberInput.classList.add("border-red-500");
+        return;
+    }
+
+    if (nomeInput.value === "") {
+        warnNome.classList.remove("hidden");
+        nomeInput.classList.add("border-red-500");
+        return;
+    }
+    
     const cartItems = cart.map((item) => {
         return (
-            ` ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} |`
+            `- ${item.name} Quantidade: (${item.quantity}) | Preço: R$${item.price} | Sabor: ${item.sabor};`
         )
-    }).join("");
-
-    const messagem = encodeURIComponent(cartItems);
+    }).join("\n");
+    
+    let opcaoPagamento = ``;
+    const totalFinal = `\nTotal: ${minhaVariavelGlobal}`; 
+    const nome = `Olá ${nomeInput.value}.`;
+    const endereco = `\n\nEndereço: ${addresInput.value}, Número: ${numberInput.value}, Complemento: ${complementoInput.value}`;
+    
+    if (pagamento === "dinheiro") {
+        opcaoPagamento = `\nForma pagamento: ${verificarPagamentoSelecionado()}, Troco para: ${trocoInput.value}`;
+    } else {
+        opcaoPagamento = `\nForma pagamento: ${verificarPagamentoSelecionado()}`;
+    }
+    const messagem = encodeURIComponent(nome + `\n\n` + cartItems + endereco + totalFinal + opcaoPagamento + `\n\nObrigado pela preferencia!`);
     const phone = "44997362393";
 
-    window.open(`https://wa.me/${phone}?text=${messagem} Endereço: ${addresInput.value}`, "_blank");
+    window.open(`https://wa.me/${phone}?text=${messagem}`, "_blank");
 
     cart = [];
     updataCartModal();
+    limpaCampos();
 })
+
+function limpaCampos() {
+    addresInput.value = "";
+    numberInput.value = "";
+    complementoInput.value = "";
+    trocoInput.value = "";
+    minhaVariavelGlobal.value = "";
+    nomeInput.value = "";
+}
 
 function checkTabacariaOpen() {
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 22;
+    return hora >= 18 && hora < 23;
 }
 
 const spanItem = document.getElementById("data-span");
@@ -200,4 +319,30 @@ if (isOpen) {
 } else {
     spanItem.classList.remove("bg-green-600");
     spanItem.classList.add("bg-red-500");
+}
+
+dinheiroRadio.addEventListener('change', function() {
+    if (this.checked) {
+        trocoInput.classList.remove('hidden');
+    }
+});
+
+// Função para esconder o campo do troco ao clicar em outras opções
+document.querySelectorAll('input[name="payment"]').forEach(function(radio) {
+    radio.addEventListener('change', function() {
+        if (!dinheiroRadio.checked) {
+            trocoInput.classList.add('hidden');
+        }
+    });
+});
+
+// Função para verificar qual opção de pagamento está marcada
+function verificarPagamentoSelecionado() {
+    const pagamentoSelecionado = document.querySelector('input[name="payment"]:checked');
+    
+    if (pagamentoSelecionado) {
+        return pagamentoSelecionado.value; // Retorna o valor selecionado
+    } else {
+        return null; // Retorna null se nenhuma opção estiver selecionada
+    }
 }
